de708c194b89ecc4b55de1f24c8cdf422cbf971b
krtomo <krrtomo@gmail.com>
2019-10-23 16:16:28 +0900

    user.controllerでアクション設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = current_user
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end    
   end
 
 end
