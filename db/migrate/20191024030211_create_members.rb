9d5ca24ea89791abe78b40e9961c40c31ff24f84
krtomo <krrtomo@gmail.com>
2019-10-24 12:31:33 +0900

    membersテーブルにカラム追加

--- db/migrate/20191024030211_create_members.rb
+++ db/migrate/20191024030211_create_members.rb
@@ -1,7 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-
+      t.references :group_id, foreign_key: true
+      t.references :user_id, foreign_key: true
       t.timestamps
     end
   end
