8958ef4ad2e0fffd9653b086c2a11f85d78009c1
krtomo <krrtomo@gmail.com>
2019-10-24 12:35:18 +0900

    rails db:migrate

--- db/migrate/20191024030211_create_members.rb
+++ db/migrate/20191024030211_create_members.rb
@@ -1,8 +1,9 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-      t.references :group_id, foreign_key: true
-      t.references :user_id, foreign_key: true
+      #xx_idではなく、xxでok
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
