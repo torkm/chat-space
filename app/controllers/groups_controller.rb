46cfbbb2a7097beeb5bc26e1894ec336552bdd79
krtomo <krrtomo@gmail.com>
2019-10-24 16:53:19 +0900

    groups#createのアクション

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,12 @@ class GroupsController < ApplicationController
   end
   
   def create
-    
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end  
   end
 
   def edit
@@ -14,5 +19,10 @@ class GroupsController < ApplicationController
 
   def update
     
+  end
+  
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
