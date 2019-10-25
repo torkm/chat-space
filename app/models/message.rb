da3a9f943f25b46a05b7fbd4f31e0e91c49e2a19
krtomo <krrtomo@gmail.com>
2019-10-25 13:57:33 +0900

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  validates :body, presence: true, unless: :image?
-  
   mount_uploader :image, ImageUploader
+
+  validates :body, presence: true, unless: :image?
 end
