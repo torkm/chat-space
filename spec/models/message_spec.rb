b8aaedfc3488ee06e884dec6487640729e58b4cf
krtomo <krrtomo@gmail.com>
2019-10-28 14:08:58 +0900

    messageモデルのテストの雛形作成

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe Message do
+  #条件なし
+  it "is valid under no conditions" do
+  end
+
+  ######### メッセージを保存できる場合 #########
+
+  # メッセージがあれば保存できる
+  it "is valid" do
+  end
+
+  # 画像があれば保存できる  
+  it "is valid" do
+  end
+
+  # メッセージと画像があれば保存できる  
+  it "is valid" do
+  end
+  
+
+
+  ####### メッセージを保存できない場合 #######
+  # メッセージも画像も無いと保存できない
+  it "is invalid" do
+  end
+  
+
+  # group_idが無いと保存できない
+  it "is invalid" do
+  end
+
+  # user_idが無いと保存できない
+  it "is invalid" do
+  end
+
+
+
+end
+
+
+
+
+
+
+
+
