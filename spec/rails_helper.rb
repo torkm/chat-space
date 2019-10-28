59c4c1f89efc0a437d9d2f3381afeff9e6778e33
krtomo <krrtomo@gmail.com>
2019-10-28 20:42:43 +0900

    capybara導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+#統合テスト用のcapybaraを使用
+require 'capybara/rspec'
+
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
