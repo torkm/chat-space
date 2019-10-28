094e2374dc8bf0991c12dfda0558fed632d4f22f
krtomo <krrtomo@gmail.com>
2019-10-28 14:58:39 +0900

    messageモデルのテスト

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,48 +1,57 @@
 require 'rails_helper'
 
 describe Message do
-  #条件なし
-  it "is valid under no conditions" do
-  end
+  describe "#create" do
 
-  ######### メッセージを保存できる場合 #########
+    ######### メッセージを保存できる場合 #########
+    context 'can save' do
+      # メッセージがあれば保存できる
+      it "is valid with only a body" do
+        message = build(:message, image: nil)
+        expect(message).to be_valid
+      end
 
-  # メッセージがあれば保存できる
-  it "is valid" do
-  end
+      # 画像があれば保存できる  
+      it "is valid with only an image" do
+        message = build(:message, body: nil)
+        expect(message).to be_valid
+      end
 
-  # 画像があれば保存できる  
-  it "is valid" do
-  end
+      # メッセージと画像があれば保存できる  
+      it "is valid with both  an image and a body" do
+        message = build(:message)
+        expect(message).to be_valid
+      end
+    end
+ 
+    ####### メッセージを保存できない場合 #######
+    context 'cannot save' do
+      # メッセージも画像も無いと保存できない
+      it "is invalid without an image or a message" do
+        message = build(:message, image: nil, body: nil)
+        message.valid?
+        expect(message.errors[:body]).to include("を入力してください")
+      end
+    
+      # group_idがないと保存できない
+      it "is invalid without an group_id" do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to include("を入力してください")
+      end
 
-  # メッセージと画像があれば保存できる  
-  it "is valid" do
-  end
+      # user_idがないと保存できない
+      it "is invalid without an user_id" do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user]).to include("を入力してください")
+      end
+
+    end
   
-
-
-  ####### メッセージを保存できない場合 #######
-  # メッセージも画像も無いと保存できない
-  it "is invalid" do
   end
-  
-
-  # group_idが無いと保存できない
-  it "is invalid" do
-  end
-
-  # user_idが無いと保存できない
-  it "is invalid" do
-  end
-
-
 
 end
 
-
-
-
-
-
-
-
+#  コンソールで確認するとき
+# message = Message.new(body:nil, image:nil, user_id:nil, group_id: nil)
