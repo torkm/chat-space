3c56336f3c081db77964fdd5b4ece184b5956d91
krtomo <krrtomo@gmail.com>
2019-10-28 13:53:00 +0900

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,5 +59,9 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  # config.filter_gems_from_backtrace("gem name")  config.include FactoryBot::Syntax::Methods
+  
+  #factorybotの記述を省略する
+  config.include FactoryBot::Syntax::Methods
+
 end
