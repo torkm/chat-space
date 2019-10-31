cd7784ea4a1dca70bba9ae46f1a5d4f49645952d
krtomo <krrtomo@gmail.com>
2019-10-31 15:27:26 +0900

    メンバーの追加削除ボタンの挙動実装

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,19 +5,32 @@ $(function () {
     var html = `
             <div class="chat-group-user clearfix">
               <p class="chat-group-user__name">${user.name}</p>
-              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+              <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
             </div>
             `
     $('#user-search-result').append(html)
-  }
+  };
   function appendUserNotFound() {
     let html = `
                <div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">ユーザーが見つかりません</p>
                </div>`
     $('#user-search-result').append(html)
-  }
+  };
 
+  function appendDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <a class="chat-group-user__btn chat-group-user__btn--remove chat-group-user__button" data-user-id="${id}" data-user-name="${name}">削除</a>
+    </div>`;
+    $(".js-add-user").append(html);
+  };
+
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  };
 
   $("#user-search-field").on("keyup", function () {
     let input = $("#user-search-field").val();
@@ -31,7 +44,7 @@ $(function () {
     })
       .done(function (users) {
         console.log("データ取得成功");
-        console.log(users);
+        // console.log(users);
         $('#user-search-result').empty();
         if (users.length !== 0) {
           users.forEach(function (user) {
@@ -50,9 +63,18 @@ $(function () {
         alert("ユーザー検索に失敗しました");
       })
   })
+  // 追加ボタンを押すと、検索結果から消えて、メンバーに追加されるように
+  $(document).on('click', '.chat-group-user__btn--add', function () {
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this).parent().remove();
+    appendDeleteUser(userName, userId);
+  })
+  // 削除を押すと、メンバーから消える (検索結果に戻るのは未実装)
+  $(document).on('click', '.chat-group-user__btn--remove', function () {
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this).parent().remove();
+  })
 });
 
-$(document).on('click', '.chat-group-user__btn--add', function (e) {
-  console.log('発火しました')
-  console.log(e)
-})
