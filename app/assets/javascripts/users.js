1b45e23c071051b77b023434312b60c4ffac0d58
krtomo <krrtomo@gmail.com>
2019-10-31 12:01:29 +0900

    合致するユーザーをインクリメンタルで表示

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,25 @@
 $(function () {
+
+
+  function appendUser(user) {
+    var html = `
+            <div class="chat-group-user clearfix">
+              <p class="chat-group-user__name">${user.name}</p>
+              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+            </div>
+            `
+    $('#user-search-result').append(html)
+  }
+  function appendUserNotFound() {
+    let html = `
+               <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザーが見つかりません</p>
+               </div>`
+    $('#user-search-result').append(html)
+  }
+
+
+
   $("#user-search-field").on("keyup", function () {
     let input = $("#user-search-field").val();
     // console.log(input)
@@ -9,11 +30,22 @@ $(function () {
       data: { keyword: input },
       dataType: 'json',
     }).done(function (users) {
-      console.log("データ取得成功")
-      console.log(users)
+      console.log("データ取得成功");
+      console.log(users);
+      $('#user-search-result').empty();
+      if (users.length !== 0) {
+        users.forEach(function (user) {
+          appendUser(user);
+        })
+      }
+      else {
+        appendUserNotFound();
+      }
+
+
     }).fail(function () {
-      console.log("データ取得失敗")
-      // alert("error")
+      console.log("データ取得失敗");
+      alert("ユーザー検索に失敗しました");
     })
   })
-})
+});
