f1e156c2b3c9c7eb32f570cd24bd6d69285568e3
krtomo <krrtomo@gmail.com>
2019-11-06 18:08:12 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,27 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock "3.11.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "chat-space"
+set :repo_url, "git@github.com:krtomo/chat-space.git"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' 
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/Chatspace.pem']  
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
