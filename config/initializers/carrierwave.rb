1ace7020786a2711a162627d793e09801911fd3b
krtomo <krrtomo@gmail.com>
2019-11-06 20:51:53 +0900

    S3アップロード

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1' 
-
+  }
 
   config.fog_directory  = 'chat-space-krtomo'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-krtomo'
