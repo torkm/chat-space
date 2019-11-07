2f8f4fb42bb4c195687efeffa3170d5bffa070d8
krtomo <krrtomo@gmail.com>
2019-11-07 10:15:13 +0900

    S3経由での画像アップロード

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,13 @@
+# secrets.yml用のシンボリックリンク
+set :linked_files, %w{ config/secrets.yml }
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # config valid for current version and patch releases of Capistrano
 lock "3.11.2"
 
@@ -20,8 +30,19 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-end
 
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
